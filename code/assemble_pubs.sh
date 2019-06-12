a62f747ae5c2dadb8dc442f0a2d1218af053a95e
Max Callaghan <max.w.callaghan@gmail.com>
2019-06-12 11:13:11 +0200

    0 draft

--- code/assemble_pubs.sh
+++ code/assemble_pubs.sh
@@ -4,14 +4,8 @@
 
 echo "assembling plots for run $1"
 
-cp tsne_results/plots/run_$1_s_0_p$2_all_topic_words_wgs.png plots_pub/all_topic_words_wgs.png
+cp tsne_results/plots/run_$1_s_0_p$2_all_topic_words_oecds.png plots_pub/all_topic_words_oecds.png
+cp tsne_results/plots/run_$1_s_0_p$2_all_topic_words_oecds.pdf plots_pub/all_topic_words_oecds.pdf
 
-cp tsne_results/plots/run_$1_s_0_p$2_all_topic_words_wgs.png plots_pub/all_topic_words_ars.png
-
-cp tsne_results/plots/run_$1_s_0_p$2_all_topic_words_wgs.png plots_pub/all_topic_words_oecds.png
-
-cp plots/ipcc_representation/ipcc_rep_new$1_all.pdf plots_pub/ipcc_represntation.pdf
-
-cp plots/ipcc_representation/ipcc_rep_oecds_time.pdf plots_pub/ipcc_rep_oecds_time.pdf
-
-cp plots/ipcc_representation/ipcc_rep_oecds_simplified.pdf plots_pub/ipcc_rep_oecds_simplified.pdf
+cp tsne_results/plots/run_$1_s_0_p$2_evolution_4.png plots_pub/topic_evolution_4.png
+cp tsne_results/plots/run_$1_s_0_p$2_evolution_4.pdf plots_pub/topic_evolution_4.pdf
